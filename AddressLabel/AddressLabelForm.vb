90e37cbf39dd40b67e2cbe3aa30892c7ea1bf23e
Payden Hoskins <paydenhoskins@gmail.com>
2025-03-23 15:14:02 -0600

    Added Code

--- AddressLabel/AddressLabelForm.vb
+++ AddressLabel/AddressLabelForm.vb
@@ -9,11 +9,14 @@ Option Explicit On
 Option Strict On
 
 Public Class AddressLabelForm
+
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Closes and ends program
         Me.Close()
         End
     End Sub
     Sub Reset()
+        'Sets all my textboxes to empty
         FirstNameTextBox.Text = ""
         LastNameTextBox.Text = ""
         StreetAddressTextBox.Text = ""
@@ -22,9 +25,13 @@ Public Class AddressLabelForm
         ZipTextBox.Text = ""
     End Sub
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
-        DisplayLabel.Text = FormatDisplayLabel()
+        'Only writes to the label if everything is filled out 
+        If UserIputIsValid() = True Then
+            DisplayLabel.Text = FormatDisplayLabel()
+        End If
     End Sub
     Function FormatDisplayLabel() As String
+        'formats the label
         Dim display As String
         display = FirstNameTextBox.Text & " " & LastNameTextBox.Text & vbNewLine _
             & StreetAddressTextBox.Text & vbNewLine _
@@ -33,7 +40,53 @@ Public Class AddressLabelForm
     End Function
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        'Clears the label and text boxes
         Reset()
         DisplayLabel.Text = ""
+    End Sub
+    Function UserIputIsValid() As Boolean
+        'Makes sure all text boxes have inputs
+        Dim Valid As Boolean = True
+        Dim message As String
+        If IsNumeric(ZipTextBox.Text) = False Then
+            Valid = False
+            ZipTextBox.Focus()
+            message &= "Please enter a valid Zip Code." & vbNewLine
+        End If
+        If StateTextBox.Text = "" Then
+            Valid = False
+            StateTextBox.Focus()
+            message &= "Please enter a valid State." & vbNewLine
+        End If
+
+        If CityTextBox.Text = "" Then
+            Valid = False
+            CityTextBox.Focus()
+            message &= "Please enter a valid City." & vbNewLine
+        End If
+        If StreetAddressTextBox.Text = "" Then
+            Valid = False
+            StreetAddressTextBox.Focus()
+            message &= "Please enter a valid address." & vbNewLine
+        End If
+        If LastNameTextBox.Text = "" Then
+            Valid = False
+            LastNameTextBox.Focus()
+            message &= "Please enter a valid last name." & vbNewLine
+        End If
+        If FirstNameTextBox.Text = "" Then
+            Valid = False
+            FirstNameTextBox.Focus()
+            message &= "Please enter a valid first name." & vbNewLine
+        End If
+        If Not Valid Then
+            MsgBox(message, MsgBoxStyle.Exclamation, "User Input Fail!")
+        End If
+        Return Valid
+    End Function
+
+    Private Sub AddressLabelForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'loads all the textboxes as empty
+        Reset()
     End Sub
 End Class
